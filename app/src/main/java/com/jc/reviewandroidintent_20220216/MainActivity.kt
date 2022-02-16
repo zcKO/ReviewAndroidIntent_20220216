5e01fc82d725f8d44b6c8eeb5373ef97bb15b367
JaeCheon <studyJcHub@gmail.com>
2022-02-16 13:06:41 +0900

    다른 액티비티로 데이터 보내기

--- app/src/main/java/com/jc/reviewandroidintent_20220216/MainActivity.kt
+++ app/src/main/java/com/jc/reviewandroidintent_20220216/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.jc.reviewandroidintent_20220216
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        moveButton.setOnClickListener {
+
+            val intent = Intent(this, OtherActivity::class.java)
+            startActivity(intent)
+
+        }
+
+        sendDataButton.setOnClickListener {
+
+            val SEND_NAME = "name"
+            val SEND_PHONE = "phone"
+
+            val sendData = userNameEditText.text.toString()
+            val phoneData = phoneEditText.text.toString()
+
+            val intent = Intent(this, OtherActivity::class.java)
+            intent.putExtra(SEND_NAME, sendData)
+            intent.putExtra(SEND_PHONE, phoneData)
+            startActivity(intent)
+
+        }
 
     }
 }
--- app/src/main/java/com/jc/reviewandroidintent_20220216/OtherActivity.kt
+++ app/src/main/java/com/jc/reviewandroidintent_20220216/OtherActivity.kt
@@ -2,10 +2,21 @@ package com.jc.reviewandroidintent_20220216
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_other.*
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+
+        val SEND_NAME = "name"
+        val SEND_PHONE = "phone"
+
+        val name = intent.getStringExtra(SEND_NAME)
+        val phone = intent.getStringExtra(SEND_PHONE)
+
+        getNameTextView.text = name
+        getPhoneTextView.text = phone
+
     }
 }
